53a7bc68e2204e3dcbb69628504257b626331280
orrzeitler <ozeitler@augury.com>
2022-03-13 15:04:52 +0200

    more pictures

--- src/Data/CategoryData.js
+++ src/Data/CategoryData.js
@@ -20,7 +20,36 @@ import kitchenFullRoomL from "../images/kitchen/kitchen_oven.jpg";
 import oven from "../images/kitchen/oven_1.jpg";
 import knives from "../images/kitchen/knives.jpg";
 
+import babyTheme from "../images/baby/baby_theme.jpg";
+import babyCrib1 from "../images/baby/baby_crib_2.jpg";
+import activityMatres from "../images/baby/activity_matress.jpg";
+import pilatesBall from "../images/baby/pilates_ball.jpg";
+import babyFoodChair from "../images/baby/baby_food_chair.jpg";
+import carChairs from "../images/baby/car_chairs.jpg";
+import babyBed from "../images/baby/baby_bed.jpg";
+import wallDecorations from "../images/baby/wall_decorations.jpg";
+import radiator from "../images/baby/radiator.jpg";
+import carry1 from "../images/baby/carry1.jpg";
+import carry2 from "../images/baby/carry2.jpg";
+import stroller from "../images/baby/stroller.jpg";
+import monitor from "../images/baby/monitor.jpg";
+import babyNetta from "../images/baby/netta.jpeg";
 
+import kidsTheme1 from "../images/kidsRoom/kids_theme1.jpg";
+import kidsTheme2 from "../images/kidsRoom/kids_theme2.jpg";
+import kidsTheme3 from "../images/kidsRoom/kids_theme3.jpg";
+import shelves from "../images/kidsRoom/shelves.jpg";
+import bedChair from "../images/kidsRoom/bed_chair.jpg";
+import lamp from "../images/kidsRoom/lamp.jpg";
+import shida from "../images/kidsRoom/shida.jpg";
+
+import officeTheme1 from "../images/office/office_theme1.jpg";
+import officeTheme2 from "../images/office/office_theme2.jpg";
+import officeChair from "../images/office/office_chair.jpg";
+import messageBoard from "../images/office/message_board.jpg";
+import magnetBoard from "../images/office/magnet_board.jpg";
+import printer from "../images/office/printer.jpg";
+import deco from "../images/office/deco.jpg";
 
 
 export const CategoryData = [
@@ -65,36 +94,52 @@ export const CategoryData = [
         categoryName: 'ציוד תינוקות',
         categoryDescription: 'כמה שהם קטנים יש להם הכי הרבה דברים',
         roomPictures: [
-            {imageSrc: livingRoom1, description: 'תיאור כלשהוא'},
-            {imageSrc: livingRoom2, description: 'עוד אפשרות לטקסט'},
+            {imageSrc: babyTheme}
         ],
         items: [
-            {id: '1', name: 'שולחן אוכל מתקפל', description: 'שולחן אוכל מאיקיאה, בלה בלה בלה', price: 200, image: table, isSold: false, isAvailableInJune: false },
-            {id: '2', name: 'כיסא', description: 'כיסא מאיקיא מדגם kjzh בצבע לבן - ישנם שתיים כאלה' , price: 50, image: chair, isSold: false, isAvailableInJune: true },
+            {id: '16', name: 'לול', description: 'לול מתקפל', price: 200, image: babyCrib1, isSold: false, isAvailableInJune: false },
+            {id: '17', name: 'משטח פעילות', description: 'משטח פעילות פאזל', price: 200, image: activityMatres, isSold: false, isAvailableInJune: false },
+            {id: '18', name: 'כדור פילאטיס', description: 'חובה לכל מוציא גראפסים מקצועי', price: 100, image: pilatesBall, isSold: false, isAvailableInJune: false },
+            {id: '19', name: 'כסא אוכל', description: 'כסא מאיקיאה', price: 70, image: babyFoodChair, isSold: false, isAvailableInJune: true },
+            {id: '20', name: 'כסאות בטיחות לאוטו', description: 'כסאות עם בין שנה ל4 שנים תקינות', price: 50, image: carChairs, isSold: false, isAvailableInJune: false },
+            {id: '21', name: 'מיטת תינוק', description: 'מיטה עםן מזרון ונחשוש', price: 200, image: babyBed, isSold: false, isAvailableInJune: true },
+            {id: '22', name: 'קישוטי קיר', description: 'ציורי יד על ידי האומנית ״חווה״', price: 50, image: wallDecorations, isSold: false, isAvailableInJune: false },
+            {id: '23', name: 'רדיאטור', description: 'רדיאטור 8 פסים' , price: 100, image: radiator, isSold: false, isAvailableInJune: false },
+            {id: '24', name: 'מנשא 1', description: 'מנשא מדגם משהו' , price: 100, image: carry1, isSold: false, isAvailableInJune: false },
+            {id: '25', name: 'מנשא 2', description: 'מנשא מדגם משהו' , price: 100, image: carry2, isSold: false, isAvailableInJune: false },
+            {id: '26', name: 'עגלה', description: 'עגלה אמבטיה + ישיבה, באה עם סט בדים להחלפה מלאה' , price: 300, image: stroller, isSold: false, isAvailableInJune: false },
+            {id: '27', name: 'מוניטור', description: 'מוניטור לשינה' , price: 100, image: monitor, isSold: false, isAvailableInJune: false },
+            {id: '28', name: 'ילדה', description: 'להשכרה בלבד לעד שלושה ימים - ילדה ממש מתוקה ביום, במהלך הלילה מתעוררת 5 פעמים, לא רוצה בקבוק ורק יונקת, אנחנו עייפים' , price: -3000, image: babyNetta, isSold: false, isAvailableInJune: false },
         ]
     },
     {
-        categoryName: 'גינה ועציצים',
-        categoryDescription: 'ילדים הורים ועוד כמה רהיטים',
+        categoryName: 'חדר ילדים',
+        categoryDescription: 'מעבר לרהיטים יש גם מלא ספרים וצעצועים - ב״אולם התצוגה״ בלבד',
         roomPictures: [
-            {imageSrc: livingRoom1, description: 'תיאור כלשהוא'},
-            {imageSrc: livingRoom2, description: 'עוד אפשרות לטקסט'},
+            {imageSrc: kidsTheme1},
+            {imageSrc: kidsTheme2},
+            {imageSrc: kidsTheme3},
         ],
         items: [
-            {id: '1', name: 'שולחן אוכל מתקפל', description: 'שולחן אוכל מאיקיאה, בלה בלה בלה', price: 200, image: table, isSold: false, isAvailableInJune: false },
-            {id: '2', name: 'כיסא', description: 'כיסא מאיקיא מדגם kjzh בצבע לבן - ישנם שתיים כאלה' , price: 50, image: chair, isSold: false, isAvailableInJune: true },
+            {id: '29', name: '5 מדפים מרובעים', description: 'מדפים מאיקיאה', price: 150, image: shelves, isSold: false, isAvailableInJune: false },
+            {id: '30', name: 'כורסה נפתחת למיטת יחיד', description: 'כורסא נפתחת למיטת יחיד, באה עם ארגז קטן', price: 200, image: bedChair, isSold: false, isAvailableInJune: false },
+            {id: '31', name: 'מנורת קריאה', description: 'מנורת דובי פנדה', price: 50, image: lamp, isSold: false, isAvailableInJune: false },
+            {id: '32', name: 'שידה', description: 'שידה למסירה', price: 0, image: shida, isSold: false, isAvailableInJune: false },
         ]
     },
     {
-        categoryName: 'תינוק',
-        categoryDescription: 'ילדים הורים ועוד כמה רהיטים',
+        categoryName: 'משרד ביתי',
+        categoryDescription: 'כל החפצים של המשרד הביתי שלנו',
         roomPictures: [
-            {imageSrc: livingRoom1, description: 'תיאור כלשהוא'},
-            {imageSrc: livingRoom2, description: 'עוד אפשרות לטקסט'},
+            {imageSrc: officeTheme1},
+            {imageSrc: officeTheme2},
         ],
         items: [
-            {id: '1', name: 'שולחן אוכל מתקפל', description: 'שולחן אוכל מאיקיאה, בלה בלה בלה', price: 200, image: table, isSold: false, isAvailableInJune: false },
-            {id: '2', name: 'כיסא', description: 'כיסא מאיקיא מדגם kjzh בצבע לבן - ישנם שתיים כאלה' , price: 50, image: chair, isSold: false, isAvailableInJune: true },
+            {id: '33', name: 'כסא משרדי', description: 'כסא משרדי מאיקאה', price: 150, image: officeChair, isSold: false, isAvailableInJune: false },
+            {id: '34', name: 'לוח שעם', description: 'לוח שעם, האייל כלול' , price: 50, image: messageBoard, isSold: false, isAvailableInJune: true },
+            {id: '35', name: 'לוח מגנטי', description: 'לוח מגנטי לתמונות הודעות' , price: 50, image: magnetBoard, isSold: false, isAvailableInJune: true },
+            {id: '36', name: 'מדפסת', description: 'מדפסת hp דגם כלשהו - מגיע עם מחסנית דיו חדשה' , price: 100, image: printer, isSold: false, isAvailableInJune: true },
+            {id: '37', name: 'ממסר deco', description: '2 מכשירי mesh של חברת deco' , price: 100, image: deco, isSold: false, isAvailableInJune: true },
         ]
     }
 ]
